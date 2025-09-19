aed81a6decce67354c78f99f12305ed51e8094ad
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-09-19 16:41:46 +0800

    Initial Commit

---
+++ src/test/java/com/kampuskor/kampuskor/KampuskorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kampuskor.kampuskor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KampuskorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
