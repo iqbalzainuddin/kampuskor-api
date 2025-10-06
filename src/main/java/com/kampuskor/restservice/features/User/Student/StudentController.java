f544e5dbce1b5c3c93d7c3fb00fa41115e3a3112
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-06 22:22:44 +0800

    feat: student controller - manage limited access to users resources for role type student

---
+++ src/main/java/com/kampuskor/restservice/features/User/Student/StudentController.java
@@ -0,0 +1,45 @@
+package com.kampuskor.restservice.features.User.Student;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kampuskor.restservice.features.User.User;
+import com.kampuskor.restservice.features.User.UserRepository;
+import com.kampuskor.restservice.features.User.dto.UsersResponse;
+import com.kampuskor.restservice.features.User.enums.RoleType;
+
+@RestController
+@RequestMapping("/students")
+class StudentController {
+    private UserRepository userRepository;
+    
+    public StudentController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    private ResponseEntity<UsersResponse> findAll(Pageable pageable) {
+        Page<User> page = userRepository.findByRoleType(
+            RoleType.S, 
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+            )
+        );
+        UsersResponse response = new UsersResponse(
+            page.getNumber(),
+            page.getNumberOfElements(),
+            page.getTotalPages(),
+            page.getTotalElements(),
+            page.getContent()
+        );
+        return ResponseEntity.ok(response);
+    }
+}
