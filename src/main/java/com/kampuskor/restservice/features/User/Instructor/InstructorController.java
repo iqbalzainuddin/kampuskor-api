17010ad6fe41250ae524c3b4ee57f94752e5c192
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-06 22:21:47 +0800

    refactor: remove unused import

--- src/main/java/com/kampuskor/restservice/features/User/Instructor/InstructorController.java
+++ src/main/java/com/kampuskor/restservice/features/User/Instructor/InstructorController.java
@@ -1,7 +1,5 @@
 package com.kampuskor.restservice.features.User.Instructor;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
