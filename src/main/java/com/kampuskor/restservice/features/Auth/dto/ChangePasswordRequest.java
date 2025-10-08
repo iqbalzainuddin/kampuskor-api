7e00ee130b29528f36bba91f593e795a43a9185c
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-09 02:57:11 +0800

    chore: add json property annotation

--- src/main/java/com/kampuskor/restservice/features/Auth/dto/ChangePasswordRequest.java
+++ src/main/java/com/kampuskor/restservice/features/Auth/dto/ChangePasswordRequest.java
@@ -1,12 +1,18 @@
 package com.kampuskor.restservice.features.Auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record ChangePasswordRequest(
+    @JsonProperty("old_password")
     @NotBlank(message = "Old password is required")
-    String oldPassword, 
+    String oldPassword,
+
+    @JsonProperty("new_password")
     @NotBlank(message = "New password is required")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character")
-    String newPassword) {
+    String newPassword
+) {
 }
