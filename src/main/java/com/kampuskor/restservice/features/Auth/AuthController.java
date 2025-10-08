1111093ca37aa35ceecd4edd4cdbf68d7ca7821f
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-08 15:58:21 +0800

    feat: add endpoint for updating profile

--- src/main/java/com/kampuskor/restservice/features/Auth/AuthController.java
+++ src/main/java/com/kampuskor/restservice/features/Auth/AuthController.java
@@ -10,15 +10,20 @@ import com.kampuskor.restservice.features.User.UserRepository;
 import com.kampuskor.restservice.utils.security.CustomUserDetails;
 import com.kampuskor.restservice.utils.security.JwtUtil;
 
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -35,18 +40,9 @@ class AuthController {
         this.jwtUtils = jwtUtils;
     }
 
-    @GetMapping("/profile")
-    private ResponseEntity<ProfileResponse> getUserById(Authentication authentication) {
-        String currentAuthenticatedUsername = authentication.getName();
-        return userRepository.findByUsernameOrEmail(currentAuthenticatedUsername, currentAuthenticatedUsername)
-            .map(user -> new ProfileResponse(user.getName(), user.getUsername(), user.getEmail()))
-            .map(ResponseEntity::ok)
-            .orElse(ResponseEntity.notFound().build());
-    }
-
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    private ResponseEntity<Void> createUser(@RequestBody RegisterRequest request) {
+    private ResponseEntity<Void> createUser(@Valid @RequestBody RegisterRequest request) {
         User user = new User();
         user.setName(request.getName());
         user.setUsername(request.getUsername());
@@ -58,15 +54,41 @@ class AuthController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    private ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest request) {
+    private ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginRequest request) {
         Authentication authenticationRequest = UsernamePasswordAuthenticationToken
                 .unauthenticated(request.getUsernameOrEmail(), request.getPassword());
         Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequest);
         CustomUserDetails userDetails = (CustomUserDetails) authenticationResponse.getPrincipal();
         if (authenticationResponse.isAuthenticated()) {
-            String token = jwtUtils.generateToken(userDetails.getUsername());
+            String token = jwtUtils.generateToken(userDetails.getId());
             return ResponseEntity.ok(new LoginResponse(token));
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/profile")
+    private ResponseEntity<ProfileResponse> getUserById(Authentication authentication) {
+        String currentAuthenticatedUsername = authentication.getName();
+        return userRepository.findByUsernameOrEmail(currentAuthenticatedUsername, currentAuthenticatedUsername)
+            .map(user -> new ProfileResponse(user.getName(), user.getUsername(), user.getEmail()))
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<Void> updateProfile(@Valid @RequestBody UpdateProfileRequest userUpdate, Authentication authentication) {
+        String currentAuthenticatedUsername = authentication.getName();
+        User user = userRepository.findByUsername(currentAuthenticatedUsername).orElse(null);
+        
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        user.setName(userUpdate.name());
+        user.setUsername(userUpdate.username());
+        user.setEmail(userUpdate.email());
+        userRepository.save(user);
+
+        return ResponseEntity.noContent().build();
     }
 }
