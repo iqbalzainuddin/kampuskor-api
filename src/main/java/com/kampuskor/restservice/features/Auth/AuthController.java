93576b8a000af371de0b70fa083370578f1f4e9a
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-09 02:20:20 +0800

    chore: remove unused import

--- src/main/java/com/kampuskor/restservice/features/Auth/AuthController.java
+++ src/main/java/com/kampuskor/restservice/features/Auth/AuthController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.http.HttpStatus;
@@ -23,10 +24,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
 @RequestMapping("/auth")
