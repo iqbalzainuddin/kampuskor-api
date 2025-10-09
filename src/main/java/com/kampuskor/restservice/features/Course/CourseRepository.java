758d1bc37026b1e64a15b093bb7383e42596f9ff
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-10 05:48:46 +0800

    feat: add course repository

---
+++ src/main/java/com/kampuskor/restservice/features/Course/CourseRepository.java
@@ -0,0 +1,32 @@
+package com.kampuskor.restservice.features.Course;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.kampuskor.restservice.features.Course.Model.Course;
+import com.kampuskor.restservice.features.User.User;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    Course findByCode(String code);
+    Page<Course> findByInstructor(User instructor, PageRequest pageRequest);
+    Page<Course> findByStudents(User student, PageRequest pageRequest);
+    Optional<Course> findByIdAndInstructor(Long id, User instructor);
+    
+    @Query("""
+        SELECT s
+        FROM Course c 
+        JOIN c.students s
+        WHERE c.id = :courseId 
+        AND c.instructor = :instructor
+    """)
+    Page<User> findStudentsByIdAndInstructor(
+        @Param("courseId") Long courseId, 
+        @Param("instructor") User instructor, 
+        PageRequest pageRequest
+    );
+}
