a57d7163d5b3fa84fa48e9234a7519803f72874f
iqbalzainuddin <iqbalz.dev@gmail.com>
2025-10-10 06:13:43 +0800

    feat: complete CRUD for course - Instructor can view own course and student - Student can view enrolled course, enroll and unenroll a course

--- src/main/java/com/kampuskor/restservice/features/Course/CourseController.java
+++ src/main/java/com/kampuskor/restservice/features/Course/CourseController.java
@@ -2,39 +2,130 @@ package com.kampuskor.restservice.features.Course;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.kampuskor.restservice.common.response.PageResponse;
+import com.kampuskor.restservice.features.Course.DTO.CourseRequest;
+import com.kampuskor.restservice.features.Course.DTO.CourseResponse;
+import com.kampuskor.restservice.features.Course.DTO.EnrollCourseResponse;
+import com.kampuskor.restservice.features.User.User;
+
+import jakarta.validation.Valid;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/courses")
 class CourseController {
-    @GetMapping
-    private ResponseEntity<Void> findAll() {
-        return ResponseEntity.ok().build();
+    private CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
     }
+
+    @GetMapping
+    private ResponseEntity<PageResponse<CourseResponse>> findAll(Pageable pageable) {
+        PageResponse<CourseResponse> courses = courseService.findAll(pageable);
+        return ResponseEntity.ok(courses);
+    }
+    
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CourseResponse> getCourse(@PathVariable Long courseId) {
+        CourseResponse response = courseService.getCourseById(courseId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    private ResponseEntity<PageResponse<CourseResponse>> findAllForCurrentInstructor(
+        Pageable pageable, 
+        Authentication authentication
+    ) {
+        PageResponse<CourseResponse> courses = courseService.getCoursesByInstructor(pageable, authentication.getName());
+        return ResponseEntity.ok(courses);
+    }
+
+    @GetMapping("/me/{courseId}/students")
+    public ResponseEntity<PageResponse<User>> getCourseStudents(
+        @PathVariable Long courseId,
+        Pageable pageable, 
+        Authentication authentication
+    ) {
+        PageResponse<User> response = courseService.getCourseStudents(courseId, pageable, authentication.getName());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me/enrolled")
+    public ResponseEntity<PageResponse<CourseResponse>> getEnrolledCourses(
+        Pageable pageable,
+        Authentication authentication
+    ) {
+        PageResponse<CourseResponse> response = courseService.getEnrolledCourses(pageable, authentication.getName());
+        return ResponseEntity.ok(response);
+    }
+    
     
     @PostMapping
-    private ResponseEntity<Void> createCourse() {
-        //TODO: process POST request
-        
-        return ResponseEntity.created(null).build();
+    private ResponseEntity<CourseResponse> createCourse(
+        @Valid @RequestBody CourseRequest course,
+        Authentication authentication,
+        UriComponentsBuilder uriBuilder
+    ) {
+        CourseResponse createdCourse = courseService.createCourse(course, authentication.getName());
+        return ResponseEntity
+            .created(
+                uriBuilder
+                    .path("/courses/{id}")
+                    .buildAndExpand(createdCourse.id())
+                    .toUri()
+            )
+            .body(createdCourse);
+    }
+
+    @PostMapping("/{courseId}/enroll")
+    public ResponseEntity<EnrollCourseResponse> enrollCourse(
+        @PathVariable Long courseId, 
+        Authentication authentication
+    ) {
+        String studentUsername = authentication.getName();
+        EnrollCourseResponse response = courseService.enrollCourse(courseId, studentUsername);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{courseId}/unenroll")
+    public ResponseEntity<Void> unenrollCourse(
+        @PathVariable Long courseId, 
+        Authentication authentication
+    ) {
+        String studentUsername = authentication.getName();
+        courseService.unenrollCourse(courseId, studentUsername);
+        return ResponseEntity.noContent().build();
     }
     
-    @PutMapping("/{id}")
-    private ResponseEntity<Void> updateCourse(@PathVariable Long id) {
-        //TODO: process PUT request
-
-        return ResponseEntity.ok().build();
+    
+    @PutMapping("/{courseId}")
+    private ResponseEntity<Void> updateCourse(
+        @PathVariable Long courseId,
+        @Valid @RequestBody CourseRequest courseRequest,
+        Authentication authentication
+    ) {
+        courseService.updateCourse(courseId, courseRequest, authentication.getName());
+        return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{id}")
-    private ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
-        //TODO: process DELETE request
-
+    @DeleteMapping("/{courseId}")
+    private ResponseEntity<Void> deleteCourse(
+        @PathVariable Long courseId,
+        Authentication authentication
+    ) {
+        courseService.deleteCourse(courseId, authentication.getName());
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/kampuskor/restservice/features/Course/CourseService.java
@@ -0,0 +1,247 @@
+package com.kampuskor.restservice.features.Course;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kampuskor.restservice.common.response.PageResponse;
+import com.kampuskor.restservice.features.Course.DTO.CourseRequest;
+import com.kampuskor.restservice.features.Course.DTO.CourseResponse;
+import com.kampuskor.restservice.features.Course.DTO.EnrollCourseResponse;
+import com.kampuskor.restservice.features.Course.Model.Course;
+import com.kampuskor.restservice.features.User.User;
+import com.kampuskor.restservice.features.User.UserRepository;
+
+@Service
+public class CourseService {
+    private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
+
+    public CourseService(CourseRepository courseRepository, UserRepository userRepository) {
+        this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<CourseResponse> findAll(Pageable pageable) {
+        Page<Course> courses = courseRepository.findAll(
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+            )
+        );
+
+        return new PageResponse<>(
+            courses.getNumber(),
+            courses.getNumberOfElements(),
+            courses.getTotalPages(),
+            courses.getTotalElements(),
+            courses.stream().map(course -> new CourseResponse(
+                course.getId(),
+                course.getName(),
+                course.getCode(),
+                course.getDescription(),
+                course.getInstructor().getId(),
+                course.getInstructor().getName(),
+                course.getCreatedAt(),
+                course.getUpdatedAt()
+            )).toList()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<CourseResponse> getCoursesByInstructor(Pageable pageable, String instructorUsername) {
+        User instructor = userRepository.findByUsername(instructorUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Instructor not found"));
+
+        Page<Course> courses = courseRepository.findByInstructor(
+            instructor,
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+            )
+        );
+
+        return new PageResponse<>(
+            courses.getNumber(),
+            courses.getNumberOfElements(),
+            courses.getTotalPages(),
+            courses.getTotalElements(),
+            courses.stream().map(course -> new CourseResponse(
+                course.getId(),
+                course.getName(),
+                course.getCode(),
+                course.getDescription(),
+                course.getInstructor().getId(),
+                course.getInstructor().getName(),
+                course.getCreatedAt(),
+                course.getUpdatedAt()
+            )).toList()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public CourseResponse getCourseById(Long courseId) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        return new CourseResponse(
+            course.getId(),
+            course.getName(),
+            course.getCode(),
+            course.getDescription(),
+            course.getInstructor().getId(),
+            course.getInstructor().getName(),
+            course.getCreatedAt(),
+            course.getUpdatedAt()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<User> getCourseStudents(Long courseId, Pageable pageable, String instructorUsername) {
+        User instructor = userRepository.findByUsername(instructorUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Instructor not found"));
+        Page<User> students = courseRepository.findStudentsByIdAndInstructor(
+            courseId, 
+            instructor, 
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+            )
+        );
+
+        return new PageResponse<>(
+            students.getNumber(),
+            students.getNumberOfElements(),
+            students.getTotalPages(),
+            students.getTotalElements(),
+            students.getContent()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<CourseResponse> getEnrolledCourses(Pageable pageable, String studentUsername) {
+        User student = userRepository.findByUsername(studentUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found"));
+
+        Page<Course> courses = courseRepository.findByStudents(
+            student,
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+            )
+        );
+
+        return new PageResponse<>(
+            courses.getNumber(),
+            courses.getNumberOfElements(),
+            courses.getTotalPages(),
+            courses.getTotalElements(),
+            courses.stream().map(course -> new CourseResponse(
+                course.getId(),
+                course.getName(),
+                course.getCode(),
+                course.getDescription(),
+                course.getInstructor().getId(),
+                course.getInstructor().getName(),
+                course.getCreatedAt(),
+                course.getUpdatedAt()
+            )).toList()
+        );
+    }
+
+    @Transactional
+    public CourseResponse createCourse(CourseRequest course, String instructorUsername) {
+        User instructor = userRepository.findByUsername(instructorUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Instructor not found"));
+
+        Course newCourse = new Course();
+        newCourse.setName(course.name());
+        newCourse.setCode(course.code());
+        newCourse.setDescription(course.description());
+        newCourse.setInstructor(instructor);
+
+        Course savedCourse = courseRepository.save(newCourse);
+
+        return new CourseResponse(
+            savedCourse.getId(),
+            savedCourse.getName(),
+            savedCourse.getCode(),
+            savedCourse.getDescription(),
+            savedCourse.getInstructor().getId(),
+            savedCourse.getInstructor().getName(),
+            savedCourse.getCreatedAt(),
+            savedCourse.getUpdatedAt()
+        );
+    }
+
+    @Transactional
+    public EnrollCourseResponse enrollCourse(Long courseId, String studentUsername) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        User student = userRepository.findByUsername(studentUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found"));
+
+        course.getStudents().add(student);
+        student.getEnrolledCourses().add(course);
+
+        courseRepository.save(course);
+        userRepository.save(student);
+
+        return new EnrollCourseResponse(
+            course.getId(),
+            course.getName(),
+            student.getId(),
+            student.getName()
+        );
+    }
+
+    @Transactional
+    public void unenrollCourse(Long courseId, String studentUsername) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        User student = userRepository.findByUsername(studentUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found"));
+
+        course.getStudents().remove(student);
+        student.getEnrolledCourses().remove(course);
+
+        courseRepository.save(course);
+        userRepository.save(student);
+    }
+
+    @Transactional
+    public void updateCourse(Long courseId, CourseRequest courseRequest, String instructorUsername) {
+        User instructor = userRepository.findByUsername(instructorUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Instructor not found"));
+
+        Course course = courseRepository.findByIdAndInstructor(courseId, instructor)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found or you are not the instructor"));
+
+        course.setName(courseRequest.name());
+        course.setCode(courseRequest.code());
+        course.setDescription(courseRequest.description());
+
+        courseRepository.save(course);
+    }
+
+    @Transactional
+    public void deleteCourse(Long courseId, String instructorUsername) {
+        User instructor = userRepository.findByUsername(instructorUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Instructor not found"));
+
+        Course course = courseRepository.findByIdAndInstructor(courseId, instructor)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found or you are not the instructor"));
+
+        courseRepository.delete(course);
+    }
+}
